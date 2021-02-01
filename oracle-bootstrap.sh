384c80cb0e7458b96cf26afb82aa43df8a629659
elicro <eliezer@ngtech.co.il>
2021-02-01 12:10:56 +0200

    Update 'oracle-bootstrap.sh'

--- oracle-bootstrap.sh
+++ oracle-bootstrap.sh
@@ -10,7 +10,7 @@ export TMPDIR
 dnf install -y rsync python3-pip vim curl wget net-tools git open-vm-tools sudo mlocate bash-completion oracle-epel-release-el8
 
 ## Making sure that the pacakge exits before installing
-dnf install -y hyperv-daemons fish htop ruby ruby-devel ruby-irb tmux procfile p7zip
+dnf install -y hyperv-daemons fish htop ruby ruby-devel ruby-irb tmux procfile p7zip p7zip-plugins
 
 mkdir -p /root/.ssh
 if [ -f "/root/.ssh/authorized_keys" ];then
